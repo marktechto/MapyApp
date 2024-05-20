3bea7f4f8c7e89e3c4e41c79506abe106981da77
MarkoTeshomeTECH <zeanbesa7066@gmail.com>
2024-05-20 11:55:18 +0300

    add render form on map

--- script.js
+++ script.js
@@ -10,15 +10,50 @@ const inputDistance = document.querySelector(".form__input--distance");
 const inputDuration = document.querySelector(".form__input--duration");
 const inputCadence = document.querySelector(".form__input--cadence");
 const inputElevation = document.querySelector(".form__input--elevation");
+let map, mapEvent;
 navigator.geolocation.getCurrentPosition(
   function (postion) {
     //  const latitude = postion.coords.latitude;
     // console.log(postion);
     const { latitude } = postion.coords;
     const { longitude } = postion.coords;
+    const coords = [latitude, longitude];
     console.log(`https://www.google.com/maps/@${latitude}${longitude}`);
+    map = L.map("map").setView(coords, 13);
+
+    L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(map);
+    map.on("click", function (mapE) {
+      mapEvent = mapE;
+      form.classList.remove("hidden");
+      inputDistance.focus();
+    });
   },
   function () {
     console.log("could not get current positon");
   }
 );
+form.addEventListener("submit", function (e) {
+  inputDuration.value =
+    inputCadence.value =
+    inputDistance.value =
+    inputElevation.value =
+      "";
+  e.preventDefault();
+  const { lat, lng } = mapEvent.latlng;
+  L.marker([lat, lng])
+    .addTo(map)
+    .bindPopup(
+      L.popup({
+        maxWidth: 250,
+        minWidth: 100,
+        autoClose: false,
+        closeOnClick: false,
+        className: "running-popup",
+      })
+    )
+    .setPopupContent("Workout")
+    .openPopup();
+});
