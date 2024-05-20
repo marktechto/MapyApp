974075c61cc9cbd40e880ed1bd83ead083435812
MarkoTeshomeTECH <zeanbesa7066@gmail.com>
2024-05-20 08:49:28 +0300

    add geolocation in js

--- script.js
+++ script.js
@@ -10,3 +10,15 @@ const inputDistance = document.querySelector(".form__input--distance");
 const inputDuration = document.querySelector(".form__input--duration");
 const inputCadence = document.querySelector(".form__input--cadence");
 const inputElevation = document.querySelector(".form__input--elevation");
+navigator.geolocation.getCurrentPosition(
+  function (postion) {
+    //  const latitude = postion.coords.latitude;
+    // console.log(postion);
+    const { latitude } = postion.coords;
+    const { longitude } = postion.coords;
+    console.log(`https://www.google.com/maps/@${latitude}${longitude}`);
+  },
+  function () {
+    console.log("could not get current positon");
+  }
+);
